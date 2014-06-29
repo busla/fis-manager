82ff629f50b2af2e2951b5ebe4111ab67b9a5703
Jón Levy <levito@gmail.com>
2014-06-29 14:45:08 +0000

    cleanup

--- tki/taekwondo/views.py
+++ tki/taekwondo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from taekwondo.models import Club, Member, Membership, Tournament
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
