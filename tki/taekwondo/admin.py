97e0f3f21c431305b01f85afb6de73757afd79e2
Jón Levy <levito@gmail.com>
2014-07-11 15:14:06 +0000

    added tournament category models

--- tki/taekwondo/admin.py
+++ tki/taekwondo/admin.py
@@ -1,21 +1,35 @@
 from django.contrib import admin
 from django.contrib.comments import Comment
 from django_summernote.admin import SummernoteModelAdmin
-from taekwondo.models import Club, Tournament, Member, Membership, TournamentFile, ClubFile, News, TournamentRegistration, TournamentResult, ResultRank
+from taekwondo.models import *
 
+class TournamentCategoryItemInline(admin.TabularInline):
+    model = TournamentCategoryItem
+
+class TournamentResultInline(admin.TabularInline):
+    model = TournamentResult
+
+class PointSystemItemInline(admin.TabularInline):
+    model = PointSystemItem
+
+class PointSystemAdmin(admin.ModelAdmin):
+    inlines = [PointSystemItemInline]
+    list_display = ('title', 'description')
+
+class TournamentCategoryItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category_type')
 
 class TournamentRegistrationInline(admin.TabularInline):
     model = TournamentRegistration
+    exclude = ('membership',)
 
 class TournamentRegistrationAdmin(admin.ModelAdmin):
+    inlines = [TournamentResultInline]
     list_display = ('member', 'tournament')
 
 class TournamentResultAdmin(admin.ModelAdmin):
     search_fields = ['registration']
     list_display = ('registration', 'rank')
-
-class TournamentResultInline(admin.TabularInline):
-    model = TournamentRegistration
 
 class ResultRankAdmin(admin.ModelAdmin):
     list_display = ('title', 'rank')
@@ -27,6 +41,9 @@ class ResultRankInline(admin.TabularInline):
 class TournamentFileInline(admin.TabularInline):
     model = TournamentFile
 
+class TournamentDivisionInline(admin.TabularInline):
+    model = TournamentDivision
+
 class ClubFileInline(admin.TabularInline):
     model = ClubFile
 
@@ -37,7 +54,7 @@ class MembershipAdmin(admin.ModelAdmin):
     list_display = ('member', 'club', 'date_joined', 'date_left')
     
 class TournamentAdmin(admin.ModelAdmin):
-    inlines = [TournamentFileInline, TournamentRegistrationInline]
+    inlines = [TournamentFileInline, TournamentRegistrationInline, TournamentDivisionInline]
     prepopulated_fields = {"slug": ("title",)}
 
 class ClubAdmin(SummernoteModelAdmin):
@@ -57,9 +74,11 @@ class NewsAdmin(SummernoteModelAdmin):
 
 
 admin.site.register(Club, ClubAdmin)
+admin.site.register(PointSystem, PointSystemAdmin)
 admin.site.register(Tournament, TournamentAdmin)
 admin.site.register(TournamentRegistration, TournamentRegistrationAdmin)
 admin.site.register(TournamentResult, TournamentResultAdmin)
+admin.site.register(TournamentCategoryItem, TournamentCategoryItemAdmin)
 admin.site.register(ResultRank, ResultRankAdmin)
 admin.site.register(Membership, MembershipAdmin)
 admin.site.register(Member, MemberAdmin)
--- tki/taekwondo/models.py
+++ tki/taekwondo/models.py
@@ -1,14 +1,30 @@
 from django.db import models
 from django.db.models import permalink
 
+MALE = 1
+FEMALE = 2
+GENDER_CHOICES = (
+        (MALE, 'Karl'),
+        (FEMALE, 'Kona'),
+    )
 
+AGE = 1
+WEIGHT = 2
+GRADE = 3
+
+CATEGORY_CHOICES = (
+    (AGE, 'Aldur'),
+    (WEIGHT, 'Þyngd'),
+    (GRADE, 'Gráða'),
+)
 
 class Member(models.Model):
+    
     name = models.CharField(max_length=200)
     ssn = models.CharField(max_length=11, primary_key=True)
     photo = models.ImageField(upload_to='members/photos', height_field=None, width_field=None, max_length=100, blank=True)
     slug = models.SlugField()
-    
+    gender = models.IntegerField(choices=GENDER_CHOICES, blank=True, null=True)
 
     def _get_active_club(self):
         try:
@@ -77,13 +93,42 @@ class Membership(models.Model):
     def __str__(self):
         return '%s (%s)' % (self.member, self.club)
 
+class PointSystem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return '%s' % self.title
+
+class PointSystemItem(models.Model):
+    title = models.CharField(max_length=200)
+    points = models.IntegerField(blank=True)
+    point_system = models.ForeignKey(PointSystem)
+
+    def __str__(self):
+        return '%s' % self.title
+
+class TournamentCategoryItem(models.Model):
+    category_type = models.IntegerField(choices=CATEGORY_CHOICES)
+    title = models.CharField(max_length=200)
+    min_value = models.IntegerField()
+    max_value = models.IntegerField()
+
+
+    def __str__(self):
+        return '%s' % self.title
+
+
 class Tournament(models.Model):
     title = models.CharField(max_length=200)
     image = models.FileField(upload_to='tournaments/images/%Y/%m', max_length=100, blank=True)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     date = models.DateField()
     slug = models.SlugField()
+    point_system = models.ForeignKey(PointSystem)
+    competitors = models.ManyToManyField(Member, through='TournamentRegistration', related_name='competitors') 
     
+
     @property
     def image_url(self):
         if self.image and hasattr(self.image, 'url'):
@@ -92,16 +137,24 @@ class Tournament(models.Model):
     def _get_registrations(self):
         regs = TournamentRegistration.objects.filter(tournament=self)
         return regs
-
+    registrations = property(_get_registrations)
+    
     def _get_results(self):
         res = TournamentResult.objects.filter(registration__tournament=self)
         return res
-    
-    registrations = property(_get_registrations)
     results = property(_get_results)
 
     def __str__(self):
         return '%s' % self.title
+
+class TournamentDivision(models.Model):
+    title = models.CharField(max_length=200)
+    tournament = models.ForeignKey(Tournament)
+    age = models.ForeignKey(TournamentCategoryItem, blank=True, null=True, related_name='age')
+    weight = models.ForeignKey(TournamentCategoryItem, blank=True, null=True, related_name='weight')
+    grade = models.ForeignKey(TournamentCategoryItem, blank=True, null=True, related_name='grade')
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+
 
 class TournamentFile(models.Model):
     attachment = models.FileField(upload_to='tournaments/attachments/%Y/%m', max_length=100, blank=True)
@@ -131,9 +184,15 @@ class NewsFile(models.Model):
 
 class TournamentRegistration(models.Model):
     member = models.ForeignKey(Member)
+    club = models.ForeignKey(Club)
     membership = models.ForeignKey(Membership, blank=True, null=True)
     tournament = models.ForeignKey(Tournament)
     registration_date = models.DateTimeField(auto_now=True, auto_now_add=True)
+    weight = models.IntegerField(blank=True, null=True)
+    grade = models.IntegerField(blank=True, null=True)
+
+    def age(self): # calculate age from member.ssn
+        pass
 
     def save(self):
         _membership = Membership.objects.get(pk = self.member.active_membership)
@@ -155,6 +214,7 @@ class ResultRank(models.Model):
 class TournamentResult(models.Model):
     registration = models.ForeignKey(TournamentRegistration)
     rank = models.ForeignKey(ResultRank)
+    result = models.ForeignKey(PointSystemItem)
 
     def __str__(self):
         return '%s - %s' % (self.registration, self.rank)
